f6a116ddeedc21a66b13f835d6f9f33fe484bca6
MichielAchten <michiel.achten@gmail.com>
2016-12-07 12:10:37 +0100

    TaakKaasServiceAndereOperaties1

--- KazenService/Controllers/KazenController.cs
+++ KazenService/Controllers/KazenController.cs
@@ -10,6 +10,10 @@ namespace KazenService.Controllers
 {
     public class KazenController : ApiController
     {
+        /// <summary>
+        /// Alle kazen lezen
+        /// </summary>
+        /// <returns>Alle kaezn</returns>
         public IHttpActionResult GetAll()
         {
             var detail = this.Request.RequestUri.AbsoluteUri + "/";
@@ -25,6 +29,11 @@ namespace KazenService.Controllers
             return this.Ok(kazen);
         }
 
+        /// <summary>
+        /// Kazen lezen volgens smaak
+        /// </summary>
+        /// <param name="smaak">De smaak van de te lezen kazen</param>
+        /// <returns>Kazen van een bepaalde smaak</returns>
         public IHttpActionResult GetBySmaak(string smaak)
         {
             smaak = smaak.ToUpper();
@@ -44,6 +53,11 @@ namespace KazenService.Controllers
             return Ok(kazen);
         }
 
+        /// <summary>
+        /// Een kaas lezen
+        /// </summary>
+        /// <param name="id">De id van de te lezen kaas</param>
+        /// <returns>De kaas met de gegeven id</returns>
         public IHttpActionResult Get(int id)
         {
             if (InMemoryDataBase.Kazen.ContainsKey(id))
@@ -53,5 +67,55 @@ namespace KazenService.Controllers
             return this.NotFound();
         }
 
+        /// <summary>
+        /// Een kaas verwijderen
+        /// </summary>
+        /// <param name="id">De id van de te verwijderen kaas</param>
+        /// <returns></returns>
+        public IHttpActionResult Delete(int id)
+        {
+            if (InMemoryDataBase.Kazen.ContainsKey(id))
+            {
+                InMemoryDataBase.Kazen.Remove(id);
+                return this.Ok();
+            }
+            return this.NotFound();
+        }
+
+        /// <summary>
+        /// Een kaas toevoegen
+        /// </summary>
+        /// <param name="kaas">De toe te voegen kaas</param>
+        /// <returns></returns>
+        public IHttpActionResult Post(Kaas kaas)
+        {
+            if (!this.ModelState.IsValid)
+            {
+                return this.BadRequest(this.ModelState);
+            }
+            var id = InMemoryDataBase.Kazen.Keys.Max() + 1;
+            kaas.ID = id;
+            InMemoryDataBase.Kazen[id] = kaas;
+            return this.Created(this.Request.RequestUri.AbsoluteUri + "/" + id, kaas);
+        }
+         /// <summary>
+         /// Een kaas wijzigen
+         /// </summary>
+         /// <param name="id">De id van de te wijzigen kaas</param>
+        /// <param name="kaas">De gewijzigde kaas</param>
+         /// <returns></returns>
+        public IHttpActionResult Put(int id, Kaas kaas)
+        {
+            if (!this.ModelState.IsValid)
+            {
+                return this.BadRequest(this.ModelState);
+            }
+            if (InMemoryDataBase.Kazen.ContainsKey(id))
+            {
+                InMemoryDataBase.Kazen[id] = kaas;
+                return this.Ok();
+            }
+            return this.NotFound();
+        }
     }
 }
--- KazenService/Models/Kaas.cs
+++ KazenService/Models/Kaas.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Runtime.Serialization;
 using System.Web;
@@ -11,10 +12,16 @@ namespace KazenService.Models
     {
         [DataMember(Name = "id", Order = 1)]
         public int ID { get; set; }
+        [Required]
+        [StringLength(50, MinimumLength = 1)]
         [DataMember(Name = "naam", Order = 2)]
         public string Naam { get; set; }
+        [Required]
+        [StringLength(50, MinimumLength = 1)]
         [DataMember(Name = "type", Order = 3)]
         public string Type { get; set; }
+        [Required]
+        [StringLength(50, MinimumLength = 1)]
         [DataMember(Name = "smaak", Order = 4)]
         public string Smaak { get; set; }
 
